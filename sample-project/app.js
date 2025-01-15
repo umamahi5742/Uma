f4864b13697fe9a7840dfc98d5b31949b9cef227
Mahesh <mahesh@gmail.com>
2025-01-15 18:37:18 +0530

    Change port to 4000

--- sample-project/app.js
+++ sample-project/app.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('Hello, World! Deployed with CI/CD Pipeline!');
 });
 
-const PORT = process.env.PORT || 3000; // Use environment variable for the port
+const PORT = process.env.PORT || 4000; // Use environment variable for the port
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
